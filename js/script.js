2e59200f38be9f7f31e80302e14a83bd79438df2
MstrSid <mstrsid@live.com>
2020-04-10 18:12:51 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#menubtn').click(function(){
+        /*$('#main-menu-vertical').css(
+            'display', 'block'
+        );*/
+        $('#main-menu-vertical').toggle(400);
+    })
+})
